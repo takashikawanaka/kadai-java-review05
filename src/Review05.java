a1a8bc8ee17557c4d7f276218899c8a14889ed9f
takashikawanaka <takashi.soccer.a@gmail.com>
2023-02-04 16:37:33 +0900

    add Task Program

---
+++ src/Review05.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Review05 {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true", "root", "Kwnk123_");
+
+            String selectSql = "SELECT * FROM person WHERE id = ?";
+            pstmt = con.prepareStatement(selectSql);
+
+            System.out.print("検索キーワードを入力してください > ");
+            int num = keyInNum();
+            pstmt.setInt(1, num);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                System.out.println(name + "\n" + age);
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class ERROR");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQL ERROR");
+            e.printStackTrace();
+        }
+    }
+
+    private static int keyInNum() {
+        int result = 0;
+        try {
+            BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+            String line = key.readLine();
+            result = Integer.parseInt(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
